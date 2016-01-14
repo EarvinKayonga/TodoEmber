16c62253881395ed9e3e3f991be520916b15d5db
EarvinBevolta <e.kayonga@bevolta.com>
2016-01-14 18:33:10 +0100

    Finalize

--- app/routes/todos/incomplete.js
+++ app/routes/todos/incomplete.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-        return this.store.filter('todo', function(todo) {
-            return !todo.get('complete');
-        });
+      return this.store.filter('todo', function(todo) {
+        return !todo.get('complete');
+      });
     },
     renderTemplate(controller, model) {
-        this.render('todos.index', {
-            model: model
-        });
+      this.render('todos.index', {
+        model: model
+      });
     }
 });
