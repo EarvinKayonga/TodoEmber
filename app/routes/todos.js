c38b21ca758700bdf4297535632fab847b9db6e0
EarvinBevolta <e.kayonga@bevolta.com>
2016-01-14 17:33:26 +0100

    Finalize

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,29 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
-    /*let todos = [
-      {
-        title: "Learn Ember",
-        complete: false,
-      },
-      {
-        title: "Solve World Hunger",
-        complete: true
-      },
-      {
-        title: "Learn Scala",
-        complete: false,
-      },
-      {
-        title: "Learn ES6",
-        complete: false,
-      },
-      {
-        title: "Be good at Maths",
-        complete: true,
-      }
-    ];*/
-    return this.store.findAll('todo');
-  }
+  model() {
+      /*let todos = [
+        {
+          title: "Learn Ember",
+          complete: false,
+        },
+        {
+          title: "Solve World Hunger",
+          complete: true
+        },
+        {
+          title: "Learn Scala",
+          complete: false,
+        },
+        {
+          title: "Learn ES6",
+          complete: false,
+        },
+        {
+          title: "Be good at Maths",
+          complete: true,
+        }
+      ];*/
+      return this.store.findAll('todo');
+    },
+    actions: {
+      createTodo(newTitle) {
+          this.store.createRecord('todo', {
+            title: newTitle,
+            complete: false
+          }).save();
+        },
+        updateTodo(todo) {
+          todo.save();
+        },
+        deleteTodo(todo) {
+          todo.destroyRecord();
+        }
+    }
 });
